38a95e12027d84647822f83795034d2f9ca2bcfa
Miloserdov Nikita <nmiloserdov09@gmail.com>
2017-02-09 19:13:31 +0000

    Clean dictionary

--- lib/application/parser/document/types/types.rb
+++ lib/application/parser/document/types/types.rb
@@ -1,5 +1,4 @@
 module Application
-  #doc.at_xpath('/ns2:purchaseNotice')
   DOCUMENT_TYPES = {
     'purchaseNotice'    => '/ns2:purchaseNotice',
     'fcsNotificationZP' => '/ns2:export/ns2:fcsNotificationZP'
